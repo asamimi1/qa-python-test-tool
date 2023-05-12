1a11867da2986c7a7ff66bcf6185220ae91f3c6f
Aarash Samimi <asamimi@Nerds-MacBook-Pro.local>
2023-05-12 11:39:23 -0700

    fixed authtoken fixture

--- fixtures.py
+++ fixtures.py
@@ -6,6 +6,7 @@ from config import *
 
 @pytest.fixture(scope="session")
 def auth_token():
-    response = requests.post(f"{DEV_BASE_URL}{MEMBERSHIP_URL}{API_TAG}/auth/signin", json={"username": EMAIL, "password": PASSWORD})
+    headers = {"x-api-key": "a09e57cc-f2ca-4d74-b627-0df941f67daf"}
+    response = requests.post(f"{DEV_BASE_URL}{MEMBERSHIP_URL}{API_TAG}/auth/signin", headers=headers, json={"username": EMAIL, "password": PASSWORD})
     token = response.json()["idToken"]
     return token
