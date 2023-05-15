e49de773a18639ae5ada3e91e84199883341d62d
Aarash Samimi <asamimi@Nerds-MacBook-Pro.local>
2023-05-15 14:29:53 -0700

    added response conditions for wallet

--- tests/test_api_wallet.py
+++ tests/test_api_wallet.py
@@ -9,4 +9,4 @@ def test_get_wallet_eth(auth_token):
     url = f"{DEV_BASE_URL}{WALLET_URL}/api/wallets/ETH"
     headers = {"Authorization": f"Bearer {auth_token}"}
     response = requests.get(url=url, headers=headers)
-    assert response.status_code == 200
+    api_response_conditions(response)
