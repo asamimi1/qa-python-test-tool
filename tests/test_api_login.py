976e9ec56c7071731adc1a5e59039ddd27b09107
Ben Darling <bendarling@Bens-MacBook-Pro.local>
2023-05-15 14:22:00 -0600

    login glabal auth

--- tests/test_api_login.py
+++ tests/test_api_login.py
@@ -5,27 +5,32 @@ import json
 from config import *
 # from fixtures import *
 
+auth_token = None
+
 def test_login_step1():
+    global auth_token
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_KEY}"
-    data = {"email" : EMAIL, "password" : PASSWORD, "returnSecureToken" : "true" }
-    response = requests.post(url=url, data=data)
+    payload = {"email" : EMAIL, "password" : PASSWORD, "returnSecureToken" : "true" }
+    response = requests.post( "POST", url, data=payload)
     responseJson = json.loads(response.text)
     auth_token = response.json()["idToken"]
     assert response.status_code == 200
 
 def test_login_step2():
+    global auth_token
     url = f"{DEV_BASE_URL}{MEMBERSHIP_URL}/api/auth-token?authenticatorPin=888087"
     headers = {"Authorization": f"Bearer {auth_token}" }
-    response = requests.get(url=url, headers=headers)
+    response = requests.get( "GET", url, headers=headers)
     responseJson = json.loads(response.text)
     auth_token = response.json()["idToken"]
     assert response.status_code == 200
 
 def test_login_step3():
+    global auth_token
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key={FIREBASE_KEY}"
-    data = { {auth_token} : f"Bearer {auth_token}", "returnSecureToken" : "true" }
+    payload = {"token" : auth_token, "returnSecureToken" : "true" }
     headers = {"Content-Type": "application/json"}
-    response = requests.post(url=url, headers=headers, data=data)
+    response = requests.post( "POST", url, headers=headers, data=payload)
     responseJson = json.loads(response.text)
     auth_token = response.json()["idToken"]
     assert response.status_code == 200
