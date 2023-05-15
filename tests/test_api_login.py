2458719571e558d5ee20a582bc1bcfb0375f0bd2
Aarash Samimi <asamimi@Nerds-MacBook-Pro.local>
2023-05-15 13:38:45 -0700

    fixes for login

--- tests/test_api_login.py
+++ tests/test_api_login.py
@@ -5,14 +5,11 @@ import json
 from config import *
 # from fixtures import *
 
-auth_token = None
-
 def test_login_step1():
     global auth_token
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_KEY}"
-    payload = {"email" : EMAIL, "password" : PASSWORD, "returnSecureToken" : "true" }
-    response = requests.post( "POST", url, data=payload)
-    responseJson = json.loads(response.text)
+    data = {"email" : EMAIL, "password" : PASSWORD, "returnSecureToken" : "true" }
+    response = requests.post(url=url, json=data)
     auth_token = response.json()["idToken"]
     assert response.status_code == 200
 
@@ -20,18 +17,16 @@ def test_login_step2():
     global auth_token
     url = f"{DEV_BASE_URL}{MEMBERSHIP_URL}/api/auth-token?authenticatorPin=888087"
     headers = {"Authorization": f"Bearer {auth_token}" }
-    response = requests.get( "GET", url, headers=headers)
-    responseJson = json.loads(response.text)
-    auth_token = response.json()["idToken"]
+    response = requests.get(url=url, headers=headers)
+    auth_token = response.json()["token"]
     assert response.status_code == 200
 
 def test_login_step3():
     global auth_token
     url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key={FIREBASE_KEY}"
-    payload = {"token" : auth_token, "returnSecureToken" : "true" }
+    data = {"token" : auth_token, "returnSecureToken" : "true" }
     headers = {"Content-Type": "application/json"}
-    response = requests.post( "POST", url, headers=headers, data=payload)
-    responseJson = json.loads(response.text)
+    response = requests.post(url, headers=headers, json=data)
     auth_token = response.json()["idToken"]
     assert response.status_code == 200
 
